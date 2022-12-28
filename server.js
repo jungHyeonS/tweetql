2282c103ac8207396f7fca2f2daf3505a3d6639e
jungHyeonS <fbtls25@naver.com>
2022-12-28 22:40:23 +0900

    feat : Query Type

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 import { ApolloServer,gql } from "apollo-server";
 
-const server = new ApolloServer({})
+
+//클라이언트가 쿼리를 조회할수있는 데이터 구조를 정의
+const typeDefs = gql`
+    type Query{
+         text : String
+    }
+`
+
+const server = new ApolloServer({typeDefs})
 
 server.listen().then(({url})=>{
     console.log(`Running on ${url}`)
