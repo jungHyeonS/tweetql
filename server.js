77d1e617a24d067b0bf6724ae122c3cf65f06f8f
jungHyeonS <fbtls25@naver.com>
2022-12-28 22:52:20 +0900

    feat : Scalar and root types

--- server.js
+++ server.js
@@ -2,9 +2,20 @@ import { ApolloServer,gql } from "apollo-server";
 
 
 //클라이언트가 쿼리를 조회할수있는 데이터 구조를 정의
+//Scalar types : String,int 와 같은 내장 타입
 const typeDefs = gql`
+    type User{
+        id:ID,
+        username:String
+    }
+    type Tweet{
+        id:ID
+        text:String
+        author:User
+    }
     type Query{
-         text : String
+        allTweets: [Tweet]
+        tweet(id: ID): Tweet
     }
 `
 
